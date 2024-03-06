d329244b2e7d5cc13b980bec21a5b51982f84857
ghiottoandrea <andreaghiotto9@gmail.com>
2024-03-06 12:56:57 +0100

    add snack-3-b5

---
+++ snack-3-b5/main.js
@@ -0,0 +1,8 @@
+//Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+reverseString("hello");
+console.log(reverseString(`hello`));
